cab2138d04d0a29a55f3fdf322d727a5415e3001
Sebastian <sebastianfox81@gmail.com>
2022-01-28 17:54:21 -0500

    Implemented removeItem function

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -3,6 +3,10 @@ const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return {...state, cart: []}
   }
+  if (action.type === 'REMOVE') {
+    return {...state, cart: state.cart.filter(item => item.id !== action.payload)}
+  }
+
 
   return state
 }
