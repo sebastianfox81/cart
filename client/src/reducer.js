7b5ecf8fb46eb0be57e7130ba242b6bdac4d80be
Sebastian <sebastianfox81@gmail.com>
2022-01-31 16:46:02 -0500

    Implemented increase amount function for each item in cart

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -3,8 +3,20 @@ const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return {...state, cart: []}
   }
+
   if (action.type === 'REMOVE') {
     return {...state, cart: state.cart.filter(item => item.id !== action.payload)}
+  }
+
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return {...cartItem, amount: cartItem.amount + 1}
+      }
+      return cartItem
+    });
+
+    return {...state, cart: tempCart}
   }
 
 
